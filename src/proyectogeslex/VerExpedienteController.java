9ed6858c211a2672ba453a6bd3df578bb3f70271
Raul-Campos <raul97cl@gmail.com>
2020-05-12 17:21:23 +0200

    Funcionalidad expedientes.

--- src/proyectogeslex/VerExpedienteController.java
+++ src/proyectogeslex/VerExpedienteController.java
@@ -6,17 +6,31 @@
 package proyectogeslex;
 
 import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
+import map.Cliente;
+import map.Expediente;
+import org.hibernate.Query;
 import org.hibernate.Session;
 
 /**
@@ -43,8 +57,6 @@ public class VerExpedienteController implements Initializable {
     @FXML
     private Button btnEliminarDoc;
     @FXML
-    private TableView<?> tableDocumentos1;
-    @FXML
     private TableColumn<?, ?> columnSentTitulo;
     @FXML
     private TableColumn<?, ?> columnSentFecha;
@@ -54,8 +66,6 @@ public class VerExpedienteController implements Initializable {
     private Button btnAnadirSent;
     @FXML
     private Button btnEliminarSent;
-    @FXML
-    private TableView<?> tableDocumentos11;
     @FXML
     private TableColumn<?, ?> columnAvisoID;
     @FXML
@@ -71,25 +81,25 @@ public class VerExpedienteController implements Initializable {
     @FXML
     private TabPane tabPanePrincipal;
     @FXML
-    private ChoiceBox<?> cbColumna;
+    private ChoiceBox<String> cbColumna;
     @FXML
     private TextField tfBusqueda;
     @FXML
     private Button btnBuscar;
     @FXML
-    private TableView<?> tableExpedientes;
+    private TableView<Expediente> tableExpedientes;
     @FXML
-    private TableColumn<?, ?> columnCodExpediente;
+    private TableColumn<Expediente, String> columnCodExpediente;
     @FXML
-    private TableColumn<?, ?> columnFechaCreacion;
+    private TableColumn<Expediente, String> columnFechaCreacion;
     @FXML
-    private TableColumn<?, ?> columnFechaCierre;
+    private TableColumn<Expediente, String> columnFechaCierre;
     @FXML
-    private TableColumn<?, ?> columnDNICliente;
+    private TableColumn<Expediente, String> columnDNICliente;
     @FXML
-    private TableColumn<?, ?> columnDNILetrado;
+    private TableColumn<Expediente, String> columnDNILetrado;
     @FXML
-    private TableColumn<?, ?> columnDNIProcurador;
+    private TableColumn<Expediente, String> columnDNIProcurador;
     @FXML
     private HBox idbajo;
     @FXML
@@ -97,14 +107,35 @@ public class VerExpedienteController implements Initializable {
     @FXML
     private Button btnBorrar;
     private Session session;
+    @FXML
+    private TableView<?> tableSentencias;
+    @FXML
+    private TableView<?> tableAvisos;
+    @FXML
+    private TableView<?> tableHoja;
+    @FXML
+    private TableColumn<?, ?> columnHojaCod;
+    @FXML
+    private TableColumn<?, ?> columnHojaEstado;
+    @FXML
+    private Button btnAnadirHoja;
+    @FXML
+    private Button btnEliminarHoja;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        columnCodExpediente.setCellValueFactory(new PropertyValueFactory<>("codigo"));
+        columnFechaCreacion.setCellValueFactory(new PropertyValueFactory<>("fechaCreacion"));
+        columnFechaCierre.setCellValueFactory(new PropertyValueFactory<>("fechaCierre"));
+        columnDNICliente.setCellValueFactory(new PropertyValueFactory<>("cliente"));
+        columnDNILetrado.setCellValueFactory(new PropertyValueFactory<>("letrado"));
+        columnDNIProcurador.setCellValueFactory(new PropertyValueFactory<>("procurador"));
+
+        cbColumna.getItems().addAll("Código", "Fecha de creación", "Fecha de cierre", "Cliente", "Letrado", "Procurador");
+    }
 
     @FXML
     private void anadirDocumento(ActionEvent event) {
@@ -132,6 +163,32 @@ public class VerExpedienteController implements Initializable {
 
     @FXML
     private void buscarExpediente(ActionEvent event) {
+
+        //Comprueba si hay una opción seleccionada
+        if (cbColumna.getValue() != null) {
+
+            //Comprueba si se ha introducido un parámetro de busqueda
+            if (tfBusqueda.getText() != null) {
+
+                List<Expediente> expedientes = consultaExpediente(cbColumna.getValue(), tfBusqueda.getText());
+
+                //Comprueba si encuentra datos relacionados con la búsqueda
+                if (!expedientes.isEmpty() && expedientes != null) {
+                    tableExpedientes.setItems(FXCollections.observableArrayList(expedientes));
+                } else {
+                    Alert alertaBuqueda = new Alert(Alert.AlertType.INFORMATION);
+                    alertaBuqueda.setHeaderText("Error de búsqueda");
+                    alertaBuqueda.setContentText("No se ha podido encontrar datos relacionados con la búsqueda realizada.");
+                    alertaBuqueda.showAndWait();
+                }
+            }
+
+        } else {
+            Alert alerta = new Alert(Alert.AlertType.INFORMATION);
+            alerta.setHeaderText("Opción no seleccionado");
+            alerta.setContentText("Porfavor seleccione un campo por el que desee realizar la búsqueda");
+            alerta.showAndWait();
+        }
     }
 
     @FXML
@@ -144,6 +201,7 @@ public class VerExpedienteController implements Initializable {
 
     public void setSession(Session session) {
         this.session = session;
+        cargarExpedientes();
     }
 
     public TabPane getTabPaneAsociado() {
@@ -153,6 +211,76 @@ public class VerExpedienteController implements Initializable {
     public TabPane getTabPanePrincipal() {
         return tabPanePrincipal;
     }
-    
-    
+
+    @FXML
+    private void anadirHoja(ActionEvent event) {
+    }
+
+    @FXML
+    private void eliminarHoja(ActionEvent event) {
+    }
+
+    //Devuelve una lista en función del campo en el que desea buscar y el valor que busca
+    private List<Expediente> consultaExpediente(String campo, String valor) {
+        Query consulta = null;
+
+        if (campo.equals("Fecha de creación") || campo.equals("Fecha de cierre")) {
+
+            //Comprueba que fecha buscar
+            if (campo.equals("Fecha de creación")) {
+                campo = "fechaCreacion";
+            } else {
+                campo = "fechaCierre";
+            }
+            Date fecha;
+
+            try {
+                fecha = new SimpleDateFormat("yyyy-MM-dd").parse(valor);
+
+                consulta = session.createQuery("from Expediente where " + campo + " = ?").setParameter(0, fecha);
+                return consulta.list();
+            } catch (ParseException ex) {
+
+                //Error al introducir la fecha
+                Alert fechaAlerta = new Alert(Alert.AlertType.INFORMATION);
+                fechaAlerta.setHeaderText("Error al buscar fecha");
+                fechaAlerta.setContentText("El texto introducido no corresponde al formato de una fecha, porfavor vuelva a intentarlo.");
+                fechaAlerta.showAndWait();
+            }
+
+        }else if (campo.equals("Código")){
+            try{
+                consulta = session.createQuery("from Expediente where codigo = ?").setParameter(0, Integer.valueOf(valor));
+                return consulta.list();
+            }catch(NumberFormatException ex){
+                
+                //error al introducir valor numérico
+                Alert codigoAlerta = new Alert(Alert.AlertType.INFORMATION);
+                codigoAlerta.setHeaderText("Error al buscar código");
+                codigoAlerta.setContentText("El valor introducido debe de ser númerico, porfavor vulve a intentarlo.");
+                codigoAlerta.showAndWait();
+            }
+        } else if (campo.equals("Cliente")) {
+            consulta = session.createQuery("select e from Expediente e JOIN e.cliente c where c.dni  = ?").setParameter(0, valor);
+            return consulta.list();
+        } else if (campo.equals("Procurador")) {
+            consulta = session.createQuery("select e from Expediente e JOIN e.procurador p where p.dniProcurador  = ?").setParameter(0, valor);
+            return consulta.list();
+        } else {
+            consulta = session.createQuery("select e from Expediente e JOIN e.letrado l where l.dniLetrado  = ?").setParameter(0, valor);
+            return consulta.list();
+        }
+        
+        return new ArrayList<Expediente>();
+    }
+
+    private void cargarExpedientes() {
+
+        //Busca todos los clientes en la base de datos
+        Query consulta = session.createQuery("from Expediente");
+        List<Expediente> expedientes = consulta.list();
+
+        //Muestra los clientes en la tabla
+        tableExpedientes.setItems(FXCollections.observableArrayList(expedientes));
+    }
 }
